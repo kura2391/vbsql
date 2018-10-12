82e9b9228bfdf64c8a85699b0b5344e428e78cda
kura2391 <bitcotral35014@yahoo.co.jp>
2018-10-12 23:17:58 +0900

    add lastinsertid

--- vbsql/Insert.vb
+++ vbsql/Insert.vb
@@ -1,6 +1,7 @@
 ﻿Public Class Insert
     Private _into As String = Nothing
     Private _columns As Hashtable = Nothing
+    Private _lastInsertId As Boolean = False
 
     'set table
     Public Function into(table As String) As Insert
@@ -13,6 +14,13 @@
         _columns = hashtable
         Return Me
     End Function
+
+    'set lastInsertId
+    Public Function lastInsertId() As Insert
+        _lastInsertId = True
+        Return Me
+    End Function
+
 
     'execute sql and return integer 
     Public Function execute(connectionString As String) As Integer
@@ -46,7 +54,10 @@
         sql = sql.Substring(0, sql.Length - 1)
         values = values.Substring(0, values.Length - 1)
 
-        sql &= ") VALUES(" & values & ")"
+        sql &= ") VALUES(" & values & ");"
+        If _lastInsertId Then
+            sql &= "SELECT SCOPE_IDENTITY();"
+        End If
         Return sql
     End Function
     'insert into test(text,number,Date) values('number','99','2018-10-10')
@@ -57,10 +68,13 @@
             Throw New Exception("no table selected")
         End If
     End Sub
+
     'check _columns
     Sub checkColumns()
         If IsNothing(_columns) OrElse _columns.Count = 0 Then
             Throw New Exception("no data set")
         End If
     End Sub
+
+
 End Class
