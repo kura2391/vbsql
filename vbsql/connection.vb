1644ebf07cf4371aa9a85dae36a5fd661109eedd
kura2391 <bitcotral35014@yahoo.co.jp>
2018-10-12 17:30:29 +0900

    first commit

---
+++ vbsql/connection.vb
@@ -0,0 +1,20 @@
+﻿Public Class Connection
+
+    Private Property _connectionString As String
+
+    Public Sub New(server As String, userId As String, password As String, initialCatalog As String)
+        _connectionString = ""
+        _connectionString &= "Server=" & server & ";"
+        _connectionString &= "User ID=" & userId & ";"
+        _connectionString &= "Password=" & password & ";"
+        _connectionString &= "Initial Catalog=" & initialCatalog
+
+
+
+    End Sub
+
+    Public Sub New(connectionString As String)
+        _connectionString = connectionString
+    End Sub
+
+End Class
