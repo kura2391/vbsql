a362394f187acdd566451ed1dcc78ce14f91f603
kura2391 <bitcotral35014@yahoo.co.jp>
2018-10-20 22:50:27 +0900

    add namespace

--- vbsql/Insert.vb
+++ vbsql/Insert.vb
@@ -1,87 +1,89 @@
-﻿Public Class Insert
-    Inherits SqlAbstract
+﻿Namespace VbSql
+    Public Class Insert
+        Inherits SqlAbstract
 
-    'save lastinsertid
-    Private _lastInsertId As Boolean = False
-
-
-    Public Sub New(connection As Connection)
-        MyBase.New(connection)
-        _prefix = "@insert"
-    End Sub
-
-    Public Sub New(connectionString As String)
-        MyBase.New(connectionString)
-        _prefix = "@insert"
-    End Sub
+        'save lastinsertid
+        Private _lastInsertId As Boolean = False
 
 
-    'set table
-    Public Function into(table As String) As Insert
-        _table = table
-        Return Me
-    End Function
+        Public Sub New(connection As Connection)
+            MyBase.New(connection)
+            _prefix = "@insert"
+        End Sub
 
-    'set insertdata
-    Public Function values(ByVal paramList As List(Of Parameter)) As Insert
-        _variables = New List(Of Parameter)(paramList)
-        Return Me
-    End Function
-
-    'set lastInsertId
-    Public Function lastInsertId() As Insert
-        _lastInsertId = True
-        Return Me
-    End Function
+        Public Sub New(connectionString As String)
+            MyBase.New(connectionString)
+            _prefix = "@insert"
+        End Sub
 
 
-    'execute sql and return integer 
-    Public Function execute() As Integer
-        check()
-        setParameterName()
-        Return _connection.execute(buildSql(), buildParameter())
-    End Function
+        'set table
+        Public Function into(table As String) As Insert
+            _table = table
+            Return Me
+        End Function
 
-    'create sql
-    Private Function buildSql()
+        'set insertdata
+        Public Function values(ByVal paramList As List(Of Parameter)) As Insert
+            _variables = New List(Of Parameter)(paramList)
+            Return Me
+        End Function
 
-        Dim sql As String = "INSERT INTO " & _table & " ("
-        Dim values As String = ""
-        For i As Integer = 0 To _variables.Count - 1
-            sql &= " " & _variables(i).getColumnName & " ,"
-            values &= " " & _variables(i).getParameterName & " ,"
-        Next
-        sql = sql.Substring(0, sql.Length - 1) & ") VALUES ("
-        sql &= values.Substring(0, values.Length - 1) & ")"
-
-        If _lastInsertId Then
-            sql &= "SELECT SCOPE_IDENTITY();"
-        End If
-        Return sql
-    End Function
-
-    Public Function buildParameter() As SqlClient.SqlParameter()
-        Dim p As New List(Of SqlClient.SqlParameter)
-        For i As Integer = 0 To _variables.Count - 1
-            p.Add(_variables(i).getSqlParameter)
-        Next
-        Return p.ToArray()
-    End Function
-
-    Private Sub check()
-        checkTable()
-        checkVariables()
-        MyBase.checkConnection()
-    End Sub
+        'set lastInsertId
+        Public Function lastInsertId() As Insert
+            _lastInsertId = True
+            Return Me
+        End Function
 
 
-    'check table
-    Private Sub checkTable()
-        If IsNothing(_table) Then
-            Throw New Exception("no table selected")
-        End If
-    End Sub
+        'execute sql and return integer 
+        Public Function execute() As Integer
+            check()
+            setParameterName()
+            Return _connection.execute(buildSql(), buildParameter())
+        End Function
+
+        'create sql
+        Private Function buildSql()
+
+            Dim sql As String = "INSERT INTO " & _table & " ("
+            Dim values As String = ""
+            For i As Integer = 0 To _variables.Count - 1
+                sql &= " " & _variables(i).getColumnName & " ,"
+                values &= " " & _variables(i).getParameterName & " ,"
+            Next
+            sql = sql.Substring(0, sql.Length - 1) & ") VALUES ("
+            sql &= values.Substring(0, values.Length - 1) & ")"
+
+            If _lastInsertId Then
+                sql &= "SELECT SCOPE_IDENTITY();"
+            End If
+            Return sql
+        End Function
+
+        Public Function buildParameter() As SqlClient.SqlParameter()
+            Dim p As New List(Of SqlClient.SqlParameter)
+            For i As Integer = 0 To _variables.Count - 1
+                p.Add(_variables(i).getSqlParameter)
+            Next
+            Return p.ToArray()
+        End Function
+
+        Private Sub check()
+            checkTable()
+            checkVariables()
+            MyBase.checkConnection()
+        End Sub
+
+
+        'check table
+        Private Sub checkTable()
+            If IsNothing(_table) Then
+                Throw New Exception("no table selected")
+            End If
+        End Sub
 
 
 
-End Class
+    End Class
+End Namespace
--- vbsql/Select.vb
+++ vbsql/Select.vb
@@ -1,99 +1,101 @@
-﻿
-Public Class [Select]
-    Inherits SqlAbstract
-    Private _select As String()
-    Private _where As New Where()
-    Private _order As String = Nothing
+﻿Namespace VbSql
+
+    Public Class [Select]
+        Inherits SqlAbstract
+        Private _select As String()
+        Private _where As New Where()
+        Private _order As String = Nothing
 
 
-    Public Sub New(connection As Connection)
-        MyBase.New(connection)
-    End Sub
+        Public Sub New(connection As Connection)
+            MyBase.New(connection)
+        End Sub
 
-    Public Sub New(connectionString As String)
-        MyBase.New(connectionString)
-    End Sub
+        Public Sub New(connectionString As String)
+            MyBase.New(connectionString)
+        End Sub
 
-    'set table
-    Public Function from(table As String) As [Select]
-        _table = table
-        Return Me
-    End Function
-    'set Columns
-    Public Function [select](Optional ByVal columns As String() = Nothing) As [Select]
-        ReDim Preserve _select(columns.Length - 1)
-        Array.Copy(columns, _select, columns.Length)
-        Return Me
-    End Function
+        'set table
+        Public Function from(table As String) As [Select]
+            _table = table
+            Return Me
+        End Function
+        'set Columns
+        Public Function [select](Optional ByVal columns As String() = Nothing) As [Select]
+            ReDim Preserve _select(columns.Length - 1)
+            Array.Copy(columns, _select, columns.Length)
+            Return Me
+        End Function
 
-    'set Where
+        'set Where
 
-    Public Function where(conditions As String, col() As Parameter) As [Select]
-        _where.add(conditions, col)
-        Return Me
-    End Function
-    Public Function where(conditions As String, col() As String) As [Select]
-        _where.add(conditions, col)
-        Return Me
-    End Function
+        Public Function where(conditions As String, col() As Parameter) As [Select]
+            _where.add(conditions, col)
+            Return Me
+        End Function
+        Public Function where(conditions As String, col() As String) As [Select]
+            _where.add(conditions, col)
+            Return Me
+        End Function
 
-    'set OrderBy
-    Public Function orderBy(order As String) As [Select]
-        _order = order
-        Return Me
-    End Function
-
-
-    'execute sql and get data as DataTable
-    Public Function execute() As DataTable
-        check()
-        Return _connection.executeSelect(buildSql, buildParameter())
-    End Function
-
-    'create sql sentence 
-    Private Function buildSql() As String
-        Dim sql As String = "SELECT "
-
-        If IsNothing(_select) Then
-            sql &= " * "
-        Else
-            For i As Integer = 0 To _select.count - 1
-                sql &= "" & _select(i) & " ,"
-            Next
-            sql = sql.Substring(0, sql.Length - 1)
-        End If
+        'set OrderBy
+        Public Function orderBy(order As String) As [Select]
+            _order = order
+            Return Me
+        End Function
 
 
-        sql &= " FROM " & _table
-        sql &= " "
-        If Not _where.isEmpty() Then
-            sql &= " WHERE "
-            sql &= _where.sql()
-        End If
-        If Not IsNothing(_order) Then
-            sql &= " ORDER BY "
-            sql &= _order
-        End If
-        Return sql
-    End Function
+        'execute sql and get data as DataTable
+        Public Function execute() As DataTable
+            check()
+            Return _connection.executeSelect(buildSql, buildParameter())
+        End Function
 
-    Private Function buildParameter() As SqlClient.SqlParameter()
-        Dim p As New List(Of SqlClient.SqlParameter)
+        'create sql sentence 
+        Private Function buildSql() As String
+            Dim sql As String = "SELECT "
 
-        p.AddRange(_where.getParamList().ToArray())
+            If IsNothing(_select) Then
+                sql &= " * "
+            Else
+                For i As Integer = 0 To _select.Count - 1
+                    sql &= "" & _select(i) & " ,"
+                Next
+                sql = sql.Substring(0, sql.Length - 1)
+            End If
 
-        Return p.ToArray()
-    End Function
 
-    'if _from is not set, throw error
-    Private Sub check()
-        checkFrom()
-        checkConnection()
-    End Sub
+            sql &= " FROM " & _table
+            sql &= " "
+            If Not _where.isEmpty() Then
+                sql &= " WHERE "
+                sql &= _where.sql()
+            End If
+            If Not IsNothing(_order) Then
+                sql &= " ORDER BY "
+                sql &= _order
+            End If
+            Return sql
+        End Function
 
-    Private Sub checkFrom()
-        If IsNothing(_table) OrElse _table.Trim() = "" Then
-            Throw New Exception("no table selected")
-        End If
-    End Sub
-End Class
+        Private Function buildParameter() As SqlClient.SqlParameter()
+            Dim p As New List(Of SqlClient.SqlParameter)
+
+            p.AddRange(_where.getParamList().ToArray())
+
+            Return p.ToArray()
+        End Function
+
+        'if _from is not set, throw error
+        Private Sub check()
+            checkFrom()
+            checkConnection()
+        End Sub
+
+        Private Sub checkFrom()
+            If IsNothing(_table) OrElse _table.Trim() = "" Then
+                Throw New Exception("no table selected")
+            End If
+        End Sub
+    End Class
+End Namespace
