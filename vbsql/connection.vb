0a67bd339acd8f12fb8ae67bdad45ebec5dcf2f7
kura2391 <bitcotral35014@yahoo.co.jp>
2019-01-18 16:53:12 +0900

    add transaction process

--- vbsql/connection.vb
+++ vbsql/connection.vb
@@ -2,6 +2,16 @@
 
     'save connectionstring
     Private _cnStr As String
+
+    'connection and Command class
+    Private _cn As SqlClient.SqlConnection
+    Private _cmd As SqlClient.SqlCommand
+
+    'save if transaction
+    Private _tran As SqlClient.SqlTransaction
+    Private _transaction As Boolean = False
+
+
     Public Sub New(connectionString As String)
         _cnStr = connectionString
     End Sub
@@ -16,23 +26,105 @@
         _cnStr = _connectionString
     End Sub
 
+    'begin transaction
+    Public Sub beginTransaction()
+        If _transaction Then
+            Throw New Exception("vbsql error: transaction already started or not closed")
+            Exit Sub
+        End If
+
+        _transaction = True
+
+        _cn = New SqlClient.SqlConnection
+        _cn.ConnectionString = _cnStr
+        _cn.Open()
+        _tran = _cn.BeginTransaction
+
+    End Sub
+
+
+
     'execute delete insert update 
     Public Function execute(sql As String, values() As SqlClient.SqlParameter) As Integer
-        Dim cn As New SqlClient.SqlConnection
-        Dim cmd As New SqlClient.SqlCommand
-        cn.ConnectionString = _cnStr
-        cmd = cn.CreateCommand
-        cmd.CommandText = sql
-        cmd.Parameters.AddRange(values)
-        cn.Open()
+        If Not _transaction Then
+            Return execNonTransaction(sql, values)
+        End If
 
-        execute = cmd.ExecuteScalar()
-
-        cn.Close()
-        cmd.Dispose()
-        cn.Dispose()
+        Try
+            'transaction process
+            _cmd = _cn.CreateCommand
+            _cmd.CommandText = sql
+            _cmd.Parameters.AddRange(values)
+            _cmd.Transaction = _tran
+            execute = _cmd.ExecuteScalar()
+            _cmd.Dispose()
+        Catch ex As Exception
+            Throw New Exception("vbsql error:" & ex.Message)
+        End Try
 
     End Function
+
+    'commit transaction data
+    Public Sub commitTransaction()
+        If Not _transaction Then
+            Throw New Exception("vbsql error: not transaction")
+        End If
+
+        _tran.Commit()
+
+
+    End Sub
+
+    'rollback 
+    Public Sub rollbackTransaction()
+        If Not _transaction Then
+            Throw New Exception("vbsql error: not transaction")
+        End If
+        If Not _tran Is Nothing Then
+            _tran.Rollback()
+        End If
+    End Sub
+
+
+
+    'close 
+    Public Sub closeTransaction()
+        If Not _transaction Then
+            Throw New Exception("vbsql error: not transaction")
+        End If
+
+        If Not _cn.State = ConnectionState.Closed Then
+            _cn.Close()
+        End If
+        _cn.Dispose()
+        _tran.Dispose()
+
+        _transaction = False
+
+    End Sub
+
+
+
+    'execute with no transaction
+    Private Function execNonTransaction(sql As String, values() As SqlClient.SqlParameter) As Integer
+        _cn = New SqlClient.SqlConnection
+        _cmd = New SqlClient.SqlCommand
+        _cn.ConnectionString = _cnStr
+        _cmd = _cn.CreateCommand
+        _cmd.CommandText = sql
+        _cmd.Parameters.AddRange(values)
+        _cn.Open()
+
+        execNonTransaction = _cmd.ExecuteScalar()
+
+        _cn.Close()
+        _cmd.Dispose()
+        _cn.Dispose()
+    End Function
+
+
+
+
 
 
     'execute select 
